18651ad3965c692e44c73c6f29a6ef56aecb6a7b
unknown <angeloudelosreyes@thelewiscollege.edu.ph>
2024-10-30 11:22:29 +0800

    first system

---
+++ resources/views/email.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email</title>
+</head>
+<body>
+    <p>Dear, {{ $email }}</p>
+    <p>A file has been sent to you, please open your Web-Based File Management and File Security System account</p>
+    <p>Thank you!</p>
+</body>
+</html>
